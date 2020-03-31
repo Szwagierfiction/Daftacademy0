b2fc906af0b5e085eac09df2ce662425a9d468ad
Szwagierfiction <szwagierfiction@gmail.com>
2020-03-31 12:40:07 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+@app.get('/')
+def hello_world():
+	return {"message": "Hello_world"}
+
+@app.get('hello/{name}')
+def hello_name(name: str):
+	return f"Hello {name}"
