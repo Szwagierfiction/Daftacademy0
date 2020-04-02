e6ef8fcd65af46eb4c112f5d96cf1ebf3d67ea8f
szwagierfiction <szwagierfiction@gmail.com>
2020-04-02 19:03:55 +0200

    zrobione zad1 test

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 # Wyklad 1 - Zadanie 1
 @app.get('/')
 def hello_world():
-	return {"message": "Hello World during the coronavirus pandemic!"}
+	return {"message": "Elo Hello World during the coronavirus pandemic!"}
 
 @app.get('/hello/{name}')
 def hello_name(name: str):
