70c940ac606381a3bf8d276a684e3892a44b199a
szwagierfiction <szwagierfiction@gmail.com>
2020-04-02 19:29:10 +0200

    zrobione zad2

--- main.py
+++ main.py
@@ -7,10 +7,20 @@ app = FastAPI()
 def hello_world():
 	return {"message": "Hello World during the coronavirus pandemic!"}
 
-@app.get('/hello/{name}')
-def hello_name(name: str):
-	return f"Hello {name}"
+# Wykład 1 - Zadanie 2
 
-@app.get('/szwagier/{name}')
-def szwagier_name(name: str):
-	return f"Szwagier {name}"
+@app.get('/method')
+def read_method(request: Request):
+    return {'method': request.method}
+
+@app.post('/method')
+def read_method(request: Request):
+    return {'method': request.method}
+
+@app.put('/method')
+def read_method(request: Request):
+    return {'method': request.method}
+
+@app.delete('/method')
+def read_method(request: Request):
+    return {'method': request.method}
