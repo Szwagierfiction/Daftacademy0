b20f28f1ee51c5a95e651fd245bff156e881aad6
szwagierfiction <szwagierfiction@gmail.com>
2020-04-30 09:35:34 +0200

    zad1

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 import aiosqlite
-import sqlite3
 
 app = FastAPI()
 
@@ -10,13 +9,23 @@ async def startup():
 
 @app.get('/tracks')
 async def read_track(page: int = 0, per_page: int = 10):
-    app.db_connection.row_factory = sqlite3.Row
+    app.db_connection.row_factory = aiosqlite.Row
     cursor = await app.db_connection.execute(f'''
     SELECT tracks.TrackId as TrackId, tracks.name as Name, tracks.albumid as AlbumId, tracks.MediaTypeId as MediaTypeId,
     tracks.GenreId as GenreId, tracks.Composer as Composer, tracks.Milliseconds as Milliseconds, tracks.Bytes as Bytes, 
     tracks.UnitPrice as UnitPrice FROM tracks WHERE TrackId >= {page}''')
     data = await cursor.fetchmany(per_page)
     return data
+
+@app.get('/tracks/composers')
+async def read_track(composer_name: str):
+    try:
+        cursor = await app.db_connection.execute(f'''
+        SELECT Name FROM Tracks WHERE Tracks.Composer = "{composer_name}" ORDER BY Tracks.name''')
+        data = await cursor.fetchall()
+        return data
+    except:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
 
 """
 import uvicorn
