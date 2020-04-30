f9fdefb97781dbbb6042673408be5eb0038c8d08
szwagierfiction <szwagierfiction@gmail.com>
2020-04-30 20:37:37 +0200

    zad1

--- main.py
+++ main.py
@@ -7,13 +7,17 @@ app = FastAPI()
 async def startup():
     app.db_connection = await aiosqlite.connect('chinook.db')
 
+@app.on_event("shutdown")
+async def shutdown():
+    await app.db_connection.close()
+
 @app.get('/tracks')
 async def read_track(page: int = 0, per_page: int = 10):
     app.db_connection.row_factory = aiosqlite.Row
     cursor = await app.db_connection.execute(f'''
     SELECT tracks.TrackId as TrackId, tracks.name as Name, tracks.albumid as AlbumId, tracks.MediaTypeId as MediaTypeId,
     tracks.GenreId as GenreId, tracks.Composer as Composer, tracks.Milliseconds as Milliseconds, tracks.Bytes as Bytes, 
-    tracks.UnitPrice as UnitPrice FROM tracks WHERE TrackId >= {page}''')
+    tracks.UnitPrice as UnitPrice FROM tracks WHERE TrackId >= {page*per_page}''')
     data = await cursor.fetchmany(per_page)
     return data
 
