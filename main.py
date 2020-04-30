868f786694233c61ed67234eebf1e3ed9dd02a69
szwagierfiction <szwagierfiction@gmail.com>
2020-04-30 08:22:06 +0200

    zad1

--- main.py
+++ main.py
@@ -11,7 +11,10 @@ async def startup():
 @app.get('/tracks')
 async def read_track(page: int = 0, per_page: int = 10):
     app.db_connection.row_factory = sqlite3.Row
-    cursor = await app.db_connection.execute(f"SELECT tracks.TrackId as TrackId, tracks.name as Name, tracks.albumid as AlbumId, tracks.MediaTypeId as MediaTypeId, tracks.GenreId as GenreId, tracks.Composer as Composer, tracks.Milliseconds as Milliseconds, tracks.Bytes as Bytes, tracks.UnitPrice as UnitPrice FROM tracks WHERE TrackId >= {page}")
+    cursor = await app.db_connection.execute(f'''
+    SELECT tracks.TrackId as TrackId, tracks.name as Name, tracks.albumid as AlbumId, tracks.MediaTypeId as MediaTypeId,
+    tracks.GenreId as GenreId, tracks.Composer as Composer, tracks.Milliseconds as Milliseconds, tracks.Bytes as Bytes, 
+    tracks.UnitPrice as UnitPrice FROM tracks WHERE TrackId >= {page}''')
     data = await cursor.fetchmany(per_page)
     return data
 
